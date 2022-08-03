e1f200cef6d55f67d47e187953ac7230d3baa1f9
Vivek Maniyar <vivekmaniyar9601@gmail.com>
2022-08-03 14:28:16 +0530

    Added ejs-demo

---
+++ ejs-demo/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+});
